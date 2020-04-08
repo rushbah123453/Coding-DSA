11878690bd82bf4fa5fa71db0f6c28c4842fd541
rushabhoswal <rushabh123453@gmail.com>
2020-04-08 22:33:23 +0530

    Try catch

--- src/companyCodingQuestion/recommended/maths/Add2Fraction.java
+++ src/companyCodingQuestion/recommended/maths/Add2Fraction.java
@@ -12,33 +12,22 @@ public class Add2Fraction {
         int den1=scanner.nextInt();
         int num2=scanner.nextInt();
         int den2=scanner.nextInt();
-
         System.out.print(num1+"/"+den1+" + "+num2+"/"+den2);
-
         addFraction(num1,den1,num2,den2);
     }
 
     private static void addFraction(int num1, int den1, int num2, int den2) {
-
         int gcdOfden1den2=gcd(den1,den2);
         int den3=(den1*den2)/gcdOfden1den2;
-
         int num3=(num1*(den3/den1))+(num2*(den3/den2));
-
         lowestFraction(num3,den3);
-
-
-
 
     }
 
     private static void lowestFraction(int num3, int den3) {
-
         int commonFactor=gcd(num3,den3);
-
         int num4=num3/commonFactor;
         int den4=den3/commonFactor;
-
         System.out.print(" = "+num4+"/"+den4);
 
 
