5f9790823b134b2fb1c35f6e354df68a719035b2
rushabhoswal <rushabh123453@gmail.com>
2020-04-30 19:35:57 +0530

    Hashing Techniques

---
+++ src/dsa/arrays/Problem16.java
@@ -0,0 +1,36 @@
+package dsa.arrays;
+
+
+import java.util.Scanner;
+
+//  7 3 5 1 5   output: -1 5 -1 5 -1
+public class Problem16 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+        int inputSize=scanner.nextInt();
+        int array[]=new int[inputSize];
+        for (int i=0;i<inputSize;i++){
+            array[i]=scanner.nextInt();
+        }
+        logic(array);
+    }
+
+    private static void logic(int[] array) {
+
+        int i=0;
+        for (;i<array.length-1;i++){
+            if (array[i]<array[i+1]){
+                array[i]=array[i+1];
+            }else {
+                array[i]=-1;
+            }
+        }
+        array[i]=-1;
+
+        for (int j=0;j<array.length;j++){
+            System.out.print(array[j]+" ");
+        }
+    }
+}
