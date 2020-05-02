539d8593c13c5adac0e0e42ea2856b369e13748f
rushabhoswal <rushabh123453@gmail.com>
2020-05-02 15:05:09 +0530

    leetcode

---
+++ src/interviewPrep/java/Collections/iterable/Collection/list/LinkedList/LinkedList.java
@@ -0,0 +1,13 @@
+package interviewPrep.java.Collections.iterable.Collection.list.LinkedList;
+
+import java.util.List;
+
+public class LinkedList {
+    public static void main(String[] args) {
+        List<Integer> list=new java.util.LinkedList<>();
+        list.add(0,1);
+        list.add(1,2);
+        list.add(0,3);
+        System.out.println(list);
+    }
+}
