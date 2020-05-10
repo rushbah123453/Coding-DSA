7e1e5b404303d9c8693dfa574cb86366ee43874a
rushabhoswal <rushabh123453@gmail.com>
2020-05-11 00:28:12 +0530

    strings

--- src/contest/leetcode/strings/Problem1.java
+++ src/contest/leetcode/strings/Problem1.java
@@ -5,13 +5,12 @@ import java.util.Scanner;
 //https://leetcode.com/problems/defanging-an-ip-address/
 public class Problem1 {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        String inputString=scanner.next();
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.next();
         System.out.println(defangIPaddr(inputString));
     }
 
-    private static String defangIPaddr(String address)
-    {
+    private static String defangIPaddr(String address) {
         return address.replace(".", "[.]");
     }
 }
---
+++ src/dsa/strings/Problem13.java
@@ -0,0 +1,56 @@
+package dsa.strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Problem13 {
+
+    public static void main(String[] args) {
+
+        mostCommonWord("",new String[]{""});
+    }
+
+
+    public static String mostCommonWord(String paragraph, String[] banned) {
+
+
+        paragraph="a, a, a, a, b,b,b,c, c";
+        banned=new String[]{"a"};
+
+        for (int i=0;i<banned.length;i++){
+            paragraph=paragraph.replace(banned[i],"").trim();
+        }
+
+        paragraph= paragraph.replaceAll("\\W+"," ");
+
+
+        String arr[]=paragraph.split(" ");
+
+        HashMap<String,Integer> hashMap=new HashMap<>();
+        for (int i=0;i<arr.length;i++){
+            if (arr[i].equals(""))
+                continue;
+            if (!hashMap.containsKey(arr[i].toLowerCase())){
+                hashMap.put(arr[i].toLowerCase(),1);
+            }else {
+                hashMap.put(arr[i].toLowerCase(),hashMap.get(arr[i].toLowerCase())+1);
+            }
+        }
+
+        int max=0;
+        String res="";
+        for (Map.Entry<String,Integer> map:hashMap.entrySet()){
+       if(max<map.getValue()){
+           max=map.getValue();
+           res=map.getKey();
+       }
+        }
+
+        System.out.println(hashMap);
+        System.out.println(res);
+
+       return res;
+
+
+    }
+}
