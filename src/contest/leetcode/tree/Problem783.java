b409407640133f81d97398cb933d5d8f6a2197d3
rushabhoswal <rushabh123453@gmail.com>
2020-05-27 00:27:54 +0530

    tree

--- src/contest/leetcode/tree/Problem783.java
+++ src/contest/leetcode/tree/Problem783.java
@@ -2,21 +2,21 @@ package contest.leetcode.tree;
 //https://leetcode.com/problems/minimum-distance-between-bst-nodes/
 public class Problem783 {
 
-    int min=Integer.MAX_VALUE,prev=-1;
+    int mins=Integer.MAX_VALUE,prev=-1;
 
     public int minDiffInBST(TreeNode root) {
 
         if(root.left!=null)
             minDiffInBST(root.left);
         if(prev!=-1)
-            min=Math.min(min,root.val-prev);
+            mins=Math.min(mins,root.val-prev);
         prev=root.val;
 
         if(root.right!=null)
             minDiffInBST(root.right);
 
 
-        return min;
+        return mins;
     }
 
 
