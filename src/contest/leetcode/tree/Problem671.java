8eed3962c5d20a58c27c1bf94e228c0a0ba30f8f
rushabhoswal <rushabh123453@gmail.com>
2020-06-01 09:34:58 +0530

    trees

--- src/contest/leetcode/tree/Problem671.java
+++ src/contest/leetcode/tree/Problem671.java
@@ -4,36 +4,34 @@ public class Problem671 {
 
 
     int first;
-    long second=Long.MAX_VALUE;
-    public int findSecondMinimumValue(TreeNode root) {
-        first=root.val;
-        helper(root);
-        System.out.println("secondSmallest="+second);
+    long second = Long.MAX_VALUE;
 
-        if(second< Long.MAX_VALUE)
+    public int findSecondMinimumValue(TreeNode root) {
+        first = root.val;
+        helper(root);
+        System.out.println("secondSmallest=" + second);
+
+        if (second < Long.MAX_VALUE)
             return (int) second;
         else
             return -1;
     }
 
 
+    public void helper(TreeNode root) {
 
-
-    public void helper(TreeNode root){
-
-        if(root==null)
+        if (root == null)
             return;
 
 
-
-        if(first<root.val && root.val<second ){
-            second=root.val;
-            System.out.println("secondSmallest= " +second);
-        }else if(first==root.val){
+        if (first < root.val && root.val < second) {
+            second = root.val;
+            System.out.println("secondSmallest= " + second);
+        } else if (first == root.val) {
             helper(root.left);
             helper(root.right);
         }
 
 
-
     }
+}
