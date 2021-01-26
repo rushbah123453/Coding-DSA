f5414f474de0dc21d294d0d46291fc5ab90e6115
Tanmay Shetty <tanmaysvshetty@gmail.com>
2021-01-26 18:22:04 +0530

    Added comments in code

--- src/algorithms/dp/Interview/LongestPathInMatrix.java
+++ src/algorithms/dp/Interview/LongestPathInMatrix.java
@@ -1,48 +1,61 @@
 package algorithms.dp.Interview;
+
 //https://www.geeksforgeeks.org/find-the-longest-path-in-a-matrix-with-given-constraints/
+//Note: selected values should be in increasing order eg. 6-7-8-9. We return 4.
+//***Not yet correct
 public class LongestPathInMatrix {
+
+    //will be used for checking up, down, left, right value
+    private static final int[][] DIRECTIONS = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     public static void main(String[] args) {
 
-        int matrix[][]={{1, 10, 9},
+        int[][] matrix = {{1, 2, 9},
                 {5, 3, 8},
                 {4, 6, 7}};
-       int ans= longestIncreasingPath(matrix);
+        int ans = longestIncreasingPath(matrix);
         System.out.println(ans);
     }
 
     public static int longestIncreasingPath(int[][] matrix) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+        if (matrix == null || matrix.length == 0) {
             return 0;
         }
-        int row=matrix.length;
-        int col=matrix[0].length;
-        int dp[][]=new int[row][col];
-        int res=0;
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                int val=dfs(i,j,matrix,dp,matrix[i][j]-1);
-                res=Math.max(res,val);
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[][] dp = new int[row][col];
+        int res = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+
+                // for each value get the max length
+                int val = dfs(i, j, matrix, dp);
+
+                // find the max length across all values
+                res = Math.max(res, val);
             }
         }
-
         return res;
     }
 
+    private static int dfs(int i, int j, int[][] matrix, int[][] dp) {
+        if (dp[i][j] > 0) return dp[i][j];
+        int max = 0;
 
-    private static int dfs(int i,int j,int[][] matrix,int[][] dp,int prev){
-        if(i<0 || i>matrix.length-1 || j<0 || j>matrix[0].length-1 || matrix[i][j]!=(prev+1) )
-            return 0;
-        if(dp[i][j]>0)return dp[i][j];
-        else{
-            int curr=matrix[i][j];
-            int up=dfs(i-1,j,matrix,dp,curr);
-            int down=dfs(i+1,j,matrix,dp,curr);
-            int left=dfs(i,j-1,matrix,dp,curr);
-            int right=dfs(i,j+1,matrix,dp,curr);
-            int val= Math.max(up,Math.max(down,Math.max(left,right)));
-            dp[i][j]=++val;
-            return val;
+        // checking all four directions (up, down, left, right)
+        for (int[] direction : DIRECTIONS) {
+            int x = direction[0] + i, y = direction[1] + j;
+            if (x > -1 && y > -1 && x < matrix.length && y < matrix[0].length
+                    && matrix[x][y] > matrix[i][j]) {
+                int longest = dfs(x, y, matrix, dp);
+
+                // getting the max value across all directions
+                max = Math.max(max, longest);
+            }
         }
+
+        // adding one for counting the element itself
+        dp[i][j] = max + 1;
+        return dp[i][j];
     }
 }
--- src/algorithms/dp/knapsack/BoundedKnapsack/CountnumberOfSubsetWithGivenDiff.java
+++ src/algorithms/dp/knapsack/BoundedKnapsack/CountnumberOfSubsetWithGivenDiff.java
@@ -3,55 +3,55 @@ package algorithms.dp.knapsack.BoundedKnapsack;
 public class CountnumberOfSubsetWithGivenDiff {
 
     public static void main(String[] args) {
-        int v[]={1,2,3,4};
-        int n=v.length;
-        int diff=1;
+        int v[] = {1, 2, 3, 4};
+        int n = v.length;
+        int diff = 1;
 
         System.out.println("");
         System.out.print("number of subset ");
-        System.out.print(countNumberOfSubsetWithDiff(v,n,diff));
+        System.out.print(countNumberOfSubsetWithDiff(v, n, diff));
         System.out.println("");
     }
 
     private static int countNumberOfSubsetWithDiff(int[] v, int n, int diff) {
-       int sum=0;
-       for (int x:v){
-           sum+=x;
-       }
+        // instead of checking for difference, code can just check if given sum s1 exists
+        //   s1 +  s2 = sum
+        //   s1 -  s2 = diff
+        //   2*s1 = sum + diff
+        //   s1 = (sum + diff) / 2
+        // as a result (sum + diff) should be even
+        int sum = 0;
+        for (int x : v) {
+            sum += x;
+        }
+        if ((diff + sum) % 2 != 0) //if diff+sum is odd , no subset possible
+            return 0;
 
-       if((diff+sum)%2!=0) //if diff+sum is odd , no subset possible
-           return 0;
+        int s1 = (diff + sum) / 2;
 
-        //   s1 +  s2 =sum
-        //   s1 -  s2=diff
-        //   2*s1=sum+diff
-        //   s1=(sum+diff)/2  where sum+diff should be mandatory even
-
-        int s1=(diff+sum)/2;
-
-       return countNumberOfSubset(v,n,s1);
+        return countNumberOfSubset(v, n, s1);
     }
 
 
     private static int countNumberOfSubset(int[] v, int n, int sum) {
-        int dp[][]=new int[n+1][sum+1];
+        int[][] dp = new int[n + 1][sum + 1];
 
-        for (int i=0;i<n+1;i++){
-            for(int j=0;j<sum+1;j++){
-                if(i==0)
-                    dp[i][j]=0;
-                if(j==0)
-                    dp[i][j]=1;
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < sum + 1; j++) {
+                if (i == 0)
+                    dp[i][j] = 0;
+                if (j == 0)
+                    dp[i][j] = 1;
             }
         }
 
 
-        for (int i=1;i<n+1;i++){
-            for(int j=0;j<sum+1;j++){
-                if (j>=v[i-1]){
-                    dp[i][j]=dp[i-1][j] + dp[i-1][j-v[i-1]];
-                }else {
-                    dp[i][j]=dp[i-1][j];
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 0; j < sum + 1; j++) {
+                if (j >= v[i - 1]) {
+                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - v[i - 1]];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
