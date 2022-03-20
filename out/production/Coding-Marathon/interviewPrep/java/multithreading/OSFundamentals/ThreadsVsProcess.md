01e0feec41a5c6eb3c5c4165d9283aec723791df
Tanmaysv <tanmaysvshetty@gmail.com>
2022-03-20 21:25:07 +0530

    Interview questions

--- out/production/Coding-Marathon/interviewPrep/java/multithreading/OSFundamentals/ThreadsVsProcess.md
+++
@@ -1,7 +0,0 @@
-1. Threads shares a lot of information between them
-2. Threads are much faster to create n destroy
-3. Context switching is faster
-4. One thread can bring down entire app
-
-1. Use process when security and stability is required
-2. No task shares information between them
--- src/Test.java
+++ src/Test.java
@@ -1,29 +1,19 @@
 import java.util.*;
 
 public class Test {
-        public static List<List<Integer>> subsets(int[] nums) {
-            List<List<Integer>> res = new ArrayList<>();
-            res.add(new ArrayList<Integer>());
-            subsetsHelper(nums, res);
-            return res;
-        }
-        public static void subsetsHelper(int[] nums, List<List<Integer>> subsets) {
-            for(int i = 0; i < nums.length; i++) {
-                int len = subsets.size();
-                for(int j = 0; j < len; j++) {
-                    List<Integer> currSubset = new ArrayList<>(subsets.get(j));
-                    currSubset.add(nums[i]);
-                    subsets.add(currSubset);
-                }
-            }
-        }
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3};
-        System.out.println("Result: " + subsets(nums).toString());
+        Map<Integer, Integer> map = new HashMap<>();
+        int res = pow(2, 6, map);
+        System.out.println("res value:" + res);
+    }
 
-        String s1 = "abcd##de";
-        List<String> l = Arrays.asList(s1);
-        System.out.println("printing list:" + l.get(0));
+    public static int pow(int num, int exp, Map<Integer, Integer> map) {
+        if(map.containsKey(exp)) return map.get(exp);
+        if(exp == 1) return num;
+        int res = exp % 2 == 0 ? pow(num, exp / 2, map) * pow(num, exp / 2, map) :
+                pow(num, exp / 2, map) * pow(num, (exp - 1) / 2, map);
+        map.put(exp, res);
+        return res;
     }
 
 }
